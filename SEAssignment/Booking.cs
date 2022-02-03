0d9d01c474b6558859165d76b12bde470130c694
Teng Siang Desmond Wee <s10198212@connect.np.edu.sg>
2022-02-04 01:24:00 +0800

    added class for : - payment - feedback - ride - customer - user - booking
    pls fill in

---
+++ SEAssignment/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEAssignment
+{
+    class Booking
+    {
+        public int referenceNumber { get; set; }
+        public Customer customer { get; set; }
+        public Driver driver { get; set; }
+        public float fare { get; set; }
+        public int pickUpPoint { get; set; }
+        public int dropOffPoint { get; set; }
+        public string distance { get; set; }
+        public DateTime date { get; set; }
+        public Ride ride { get; set; }
+
+
+        //Implementation
+        public void generateReceipt() { }
+    }
+}
---
+++ SEAssignment/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEAssignment
+{
+    class User
+    {
+        public string name { get; set; }
+        public int contactNumber { get; set; }
+        public string emailAddress { get; set; }
+        public int identificationNumber { get; set; }
+    }
+}
