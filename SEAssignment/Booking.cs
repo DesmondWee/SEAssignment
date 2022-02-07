ac9a84cc7f91a83e2676edaeb4ae0f9c87737a27
Si Min Nicole Sim <s10196790@connect.np.edu.sg>
2022-02-07 17:57:35 +0800

    MISTAKE
    i coded for vehicle by accident

--- SEAssignment/Booking.cs
+++ SEAssignment/Booking.cs
@@ -24,10 +24,61 @@ namespace SEAssignment
 
         public static void MakeBooking() 
         {
-            Console.Write("");
+            //date
+            Console.Write("Enter Ride date (DD/MM/YYYY): ");
+            string datestr = Console.ReadLine();
+            DateTime date = Convert.ToDateTime(datestr);
+            //locations
+            List<String> pickupdestList = EnterPickupLocation();
+            //select vehicle goes here
+            
+        }
+        public static List<string> EnterPickupLocation() 
+        {
+            List<string> locationList = new List<string>();
+            Console.Write("Enter Pickup Location: ");
             string pickup = Console.ReadLine();
+            locationList.Add(pickup);
             Console.Write("Enter Destination: ");
             string destination = Console.ReadLine();
+            locationList.Add(destination);
+
+            return (locationList);
+        }
+
+        public static string SelectVehicle() 
+        {
+            string vehicle ="";
+            bool selectvalid = false;
+            while (selectvalid == false) 
+            {
+                Console.WriteLine("Select A Vehicle from the list below by number: \n [1] Car \n [2] Bus \n [3] Van");
+                string vehicletype = Console.ReadLine();
+                if (vehicletype.CompareTo("1") == 0)
+                {
+                    vehicle = "Car";
+                    selectvalid = true;
+                    return (vehicle);
+                }
+                else if (vehicletype.CompareTo("2") == 0)
+                {
+                    vehicle = "Bus";
+                    selectvalid = true;
+                }
+                else if (vehicletype.CompareTo("3") == 0)
+                {
+                    vehicle = "Van";
+                    selectvalid = true;
+                }
+                else { Console.WriteLine("Not a valid input! Enter the corresponding number."); } 
+            }
+
+            
+            int amount = 20;
+            Console.WriteLine("Upfront deposit amount of ${0} is required for {1}", amount, vehicle);
+
+
+            return (vehicle);
 
         }
     }
--- SEAssignment/Vehicle.cs
+++ SEAssignment/Vehicle.cs
@@ -6,5 +6,25 @@ namespace SEAssignment
 {
     class Vehicle
     {
+        private string licensePlate;
+        public string LicensePlate
+        {
+            get { return licensePlate; }
+            set { licensePlate = LicensePlate; }
+        }
+        private string brand;
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = Brand; }
+        }
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = Model; }
+        }
+
+        public Vehicle() { }
     }
 }
