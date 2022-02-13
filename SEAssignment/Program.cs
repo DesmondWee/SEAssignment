b59c55848726d70f5241e075fe8b40fca0c8f84c
Zheng Wen Hing <s10196494@connect.np.edu.sg>
2022-02-13 17:05:26 +0800

    a

--- SEAssignment/Program.cs
+++ SEAssignment/Program.cs
@@ -9,7 +9,7 @@ namespace SEAssignment
         static void Main(string[] args)
         {
             Customer tempCustomer = new Customer();
-            Car tempCar = new Car("ABC", "brand", "model");
+            Car tempCar = new Car("ABCPlace", "Speedster", "SuperSpeeder");
             List<Ride> ridelist = new List<Ride>();
             Car.carlist.Add(tempCar);
             bool exit = false;
